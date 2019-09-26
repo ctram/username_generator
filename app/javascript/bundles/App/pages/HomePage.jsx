dd3e1cde50169f4dffb93c08fe4d8a3cd6d60786
Chris Tram <ctram@protonmail.com>
2019-09-25 22:53:54 -0700

    makes basic XHR request to server

--- app/javascript/bundles/App/pages/HomePage.jsx
+++ app/javascript/bundles/App/pages/HomePage.jsx
@@ -1,9 +1,17 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import PropTypes from "prop-types";
+import React from "react";
 
-import '../styles/page.css';
+import "../styles/page.css";
 
 export default class HomePage extends React.Component {
+  componentDidMount() {
+    fetch("http://localhost:3001/api/v1/something_random", {
+      method: "GET",
+    }).then(res => {
+      console.log(res);
+    });
+  }
+
   render() {
     return (
       <div className="page page-home">
