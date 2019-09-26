23ba312d3bee03c866c3440d4da2a6e551feec74
Chris Tram <ctram@protonmail.com>
2019-09-25 22:03:50 -0700

    clean up; stub home page content.

---
+++ app/javascript/bundles/App/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import '../styles/page.css';
+
+export default class HomePage extends React.Component {
+  render() {
+    return (
+      <div className="page page-home">
+        <h1>Username Generator</h1>
+      </div>
+    );
+  }
+}
