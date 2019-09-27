af64f5082ac322a739c1771cfc5d2ddcae5ac4fb
Chris Tram <ctram@protonmail.com>
2019-09-26 17:10:13 -0700

    adds styling for username element

--- app/javascript/bundles/App/pages/HomePage.jsx
+++ app/javascript/bundles/App/pages/HomePage.jsx
@@ -2,6 +2,7 @@ import PropTypes from "prop-types";
 import React from "react";
 
 import "../styles/page.css";
+import "../styles/page-home.css";
 
 export default class HomePage extends React.Component {
   componentDidMount() {
